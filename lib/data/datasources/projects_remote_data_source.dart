bcf2f162f4c6c783179ba30f9529da54323d8d6f
yonelyvan <yonelyvan@gmail.com>
2025-06-20 12:27:21 -0500

    Feat: first designs

--- lib/data/datasources/projects_remote_data_source.dart
+++
@@ -1,5 +0,0 @@
-import 'package:app/data/models/project_model.dart';
-
-abstract class ProjectsRemoteDataSource {
-  Future<List<ProjectModel>> getProjects();
-}
--- lib/domain/repositories/projects_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:app/domain/entities/project.dart';
-
-abstract class ProjectsRepository {
-  /// Returns a [List] of [Project]
-  Future<List<Project>> getProjects();
-}
